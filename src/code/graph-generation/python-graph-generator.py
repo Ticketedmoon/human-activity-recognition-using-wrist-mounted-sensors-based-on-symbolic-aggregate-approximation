d0eddf09230684ecafa2aeefabfbbef2c0c96d4e
Shane Creedon <Shane.creedon3@mail.dcu.ie>
2018-12-27 16:26:20 +0000

    Update Graph Generation Script

--- src/code/graph-generation/python-graph-generator.py
+++ src/code/graph-generation/python-graph-generator.py
@@ -1,5 +1,20 @@
+# Import matplotlib library
+import matplotlib
+
+# Switch backend from tkinter to agg
+matplotlib.use('agg')
+
 # Import graphing tools and libraries
 import matplotlib.pyplot as plt
-import pandas as pd
+plt.style.use('classic')
 
-# Initialize Figure and Axes object
+import pandas as pd
+import numpy as np
+import seaborn as sns
+sns.set()
+
+# Load in data from PPG datasets (../../resources/exercise-datasets)
+# Contains 15000+ rows, 2 columns.
+fmri = sns.load_dataset("fmri")
+ax = sns.lineplot(x="time", y="wrist_ppg", data=fmri)
+
