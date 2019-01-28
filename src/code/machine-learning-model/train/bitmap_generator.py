68c6c273ea3f22443bb4169f52fb7eed32458ee0
Shane Creedon <Shane.creedon3@mail.dcu.ie>
2019-01-28 23:32:18 +0000

    Bitmap Generation Done -- grayscale images generation

---
+++ src/code/machine-learning-model/train/bitmap_generator.py
@@ -0,0 +1,35 @@
+from sax_transformation import Sax
+import text_to_image
+
+
+class BitmapGenerator:
+
+    bitmap_strings = []
+
+    def __init__(self):
+        self.sax_transform = Sax()
+        self.build_bitmap_strings()
+
+    def build_bitmap_strings(self):
+        walk_str, run_str = self.sax_transform.apply_sax_transformation()
+        self.bitmap_strings.append(walk_str)
+        self.bitmap_strings.append(run_str)
+
+    def generate_bitmaps(self):
+        count = 0
+        for activity_string in self.bitmap_strings:
+            print(activity_string)
+            for i in range(0, len(activity_string)//32, 1):
+                image_source = activity_string[32*i:32*(i+1)]
+                print("Image Letter Scheme #" + str(count) + ": " + image_source)
+                text_to_image.encode(image_source, "../bitmaps/activity" + str(count) + ".png")
+                count += 1
+
+
+def main():
+    generator = BitmapGenerator()
+    generator.generate_bitmaps()
+
+
+if __name__ == "__main__":
+    main()
--- src/code/machine-learning-model/train/sax_transformation.py
+++ src/code/machine-learning-model/train/sax_transformation.py
@@ -1,14 +1,14 @@
-import numpy as np
 import pandas as pd
 
 from saxpy.znorm import znorm
 from saxpy.sax import ts_to_string
 from saxpy.alphabet import cuts_for_asize
 
+
 class Sax:
 
-    walk_csv_path = "../../resources/exercise-datasets/Walk01.csv"
-    run_csv_path = "../../resources/exercise-datasets/Run01.csv"
+    walk_csv_path = "../../../resources/exercise-datasets/Walk01.csv"
+    run_csv_path = "../../../resources/exercise-datasets/Run01.csv"
 
     def __init__(self):
         self.walk_data = pd.read_csv(self.walk_csv_path, index_col=0, parse_dates=True, skiprows=[0, 1])
@@ -17,9 +17,6 @@ class Sax:
     def convert_to_single_dim(self):
         walk_stats = pd.Series(self.walk_data.values.squeeze())
         run_stats = pd.Series(self.run_data.values.squeeze())
-
-        print(walk_stats)
-        print(run_stats)
 
         walk_data = pd.to_numeric(walk_stats, errors='coerce')
         run_data = pd.to_numeric(run_stats, errors='coerce')
@@ -35,10 +32,12 @@ class Sax:
         run_results = ts_to_string(znorm(run_data), cut_size)
         return walk_results, run_results
 
+
 def main():
     sax_obj = Sax()
     walk_res, run_res = sax_obj.apply_sax_transformation()
     print_demo(walk_res, run_res)
+
 
 def print_demo(walk_results, run_results):
     print("======= WALK RESULTS =========")
@@ -46,6 +45,7 @@ def print_demo(walk_results, run_results):
     print()
     print("======= RUN RESULTS ========")
     print(run_results)
+
 
 if __name__ == "__main__":
     main()
