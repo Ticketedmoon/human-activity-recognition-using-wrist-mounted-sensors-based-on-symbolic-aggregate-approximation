e4454b0a6400f2b057d92e39a1d385b4dccfd1fc
Shane Creedon <Shane.creedon3@mail.dcu.ie>
2019-01-28 19:16:18 +0000

    Improve sax.py + apply OOP practices + remove header row outlier in data

--- src/code/machine-learning-model/sax.py
+++ src/code/machine-learning-model/sax.py
@@ -5,23 +5,43 @@ from saxpy.znorm import znorm
 from saxpy.sax import ts_to_string
 from saxpy.alphabet import cuts_for_asize
 
-walk_csv_path = "../../resources/exercise-datasets/Walk01.csv"
-run_csv_path = "../../resources/exercise-datasets/Run01.csv"
+class Sax:
 
-walk_data = pd.read_csv(walk_csv_path, index_col=0, parse_dates=True)
-run_data = pd.read_csv(run_csv_path, index_col=0, parse_dates=True)
+    walk_csv_path = "../../resources/exercise-datasets/Walk01.csv"
+    run_csv_path = "../../resources/exercise-datasets/Run01.csv"
 
-walk_stats = pd.Series(walk_data.values.squeeze())
-run_stats = pd.Series(run_data.values.squeeze())
+    def __init__(self):
+        self.walk_data = pd.read_csv(self.walk_csv_path, index_col=0, parse_dates=True, skiprows=[0])
+        self.run_data = pd.read_csv(self.run_csv_path, index_col=0, parse_dates=True, skiprows=[0])
 
-walk_data = pd.to_numeric(walk_stats, errors='coerce')
-run_data = pd.to_numeric(run_stats, errors='coerce')
+    def convert_to_single_dim(self):
+        walk_stats = pd.Series(self.walk_data.values.squeeze())
+        run_stats = pd.Series(self.run_data.values.squeeze())
 
-walk_results = ts_to_string(znorm(walk_data), cuts_for_asize(5))
-run_results = ts_to_string(znorm(run_data), cuts_for_asize(5))
+        print(walk_stats)
+        print(run_stats)
 
-print("======= WALK RESULTS =========")
-print(walk_results)
-print("\n\n\n")
-print("======= RUN RESULTS ========")
-print(run_results)
+        walk_data = pd.to_numeric(walk_stats, errors='coerce')
+        run_data = pd.to_numeric(run_stats, errors='coerce')
+        return walk_data, run_data
+
+    def apply_sax_transformation(self):
+        walk_data, run_data = self.convert_to_single_dim()
+        walk_results = ts_to_string(znorm(walk_data), cuts_for_asize(5))
+        run_results = ts_to_string(znorm(run_data), cuts_for_asize(5))
+        return walk_results, run_results
+
+def main():
+    sax_obj = Sax()
+    walk_res, run_res = sax_obj.apply_sax_transformation()
+    print_demo(walk_res, run_res)
+
+def print_demo(walk_results, run_results):
+    print("======= WALK RESULTS =========")
+    print(walk_results)
+    print()
+    print("======= RUN RESULTS ========")
+    print(run_results)
+
+if __name__ == "__main__":
+    main()
