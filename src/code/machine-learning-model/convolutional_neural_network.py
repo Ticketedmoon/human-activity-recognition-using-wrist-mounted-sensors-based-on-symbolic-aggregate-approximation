7abff3b7028889ae7ca94ced8e8aac9205c7de97
Shane Creedon <Shane.creedon3@mail.dcu.ie>
2019-01-30 15:03:05 +0000

    Start Convolutional NN research + implementation

---
+++ src/code/machine-learning-model/convolutional_neural_network.py
@@ -0,0 +1,7 @@
+"""
+The process of building a Convolutional Neural Network always involves four major steps.
+Step - 1 : Convolution
+Step - 2 : Pooling
+Step - 3 : Flattening
+Step - 4 : Full connection
+"""
