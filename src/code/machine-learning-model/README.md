2bcce153cf610e0526599c178e8b0b3b3a8ac5f9
Shane <shane.creedon3@mail.dcu.ie>
2019-01-30 16:42:35 +0000

    - Clean Walk Dataset - Fix bitmap generation (previously had a duplicate for each image) - Update readme explaining the train/test split (75/25) - Update directory structure for image files
    Signed-off-by: Shane Creedon <Shane.creedon3@mail.dcu.ie>

--- src/code/machine-learning-model/README.md
+++ src/code/machine-learning-model/README.md
@@ -1,7 +1,11 @@
 #### SAX Information
 Among transforming the exercise data sets into string representations using SAX,  
 we then must convert these strings into appropriate bitmap representations.  
-The plan is to convert **32 letters from the string into a 32 pixel bitmap image** and repeat  
-this process until all bitmaps have been correctly generated.  
-A 32 bitmap image will account for **12.5 seconds** in the data, such that we can have a  
-total of **288 images**
+The plan is to convert **10 letters from the string into a 64 pixel bitmap image** and repeat  
+this process until all bitmaps have been correctly generated. 
+ 
+A 64 bitmap image will account for **25 seconds** in the data.  
+In total for training and testing our model:
+
+*Training image data set total:* **12,555**  
+*Testing image data set total:* **3,145**
--- src/code/machine-learning-model/bitmap_generator.py
+++ src/code/machine-learning-model/bitmap_generator.py
@@ -5,34 +5,37 @@ class BitmapGenerator:
 
     def __init__(self):
         self.sax_obj = SymbolicAggregateApproximation()
-        self.generate_bitmaps()
 
     def generate_bitmaps(self):
-        try:
-            for index in range(1, 7):
+        for index in range(1, 7):
+            try:
                 walk_sax_str = self.sax_obj.generate_walk(index)
-                self.generate("Walk", walk_sax_str)
+                self.generate("Walk", walk_sax_str, index)
 
                 run_sax_str = self.sax_obj.generate_run(index)
-                self.generate("Run", run_sax_str)
+                self.generate("Run", run_sax_str, index)
 
                 low_bike_sax_str = self.sax_obj.generate_low_bike(index)
-                self.generate("LowResistanceBike", low_bike_sax_str)
+                self.generate("LowResistanceBike", low_bike_sax_str, index)
 
                 high_bike_sax_str = self.sax_obj.generate_high_bike(index)
-                self.generate("HighResistanceBike", high_bike_sax_str)
+                self.generate("HighResistanceBike", high_bike_sax_str, index)
 
-        except FileNotFoundError:
-            print("File not found")
+            except FileNotFoundError:
+                print("File not found with ID: (" + str(index) + ")")
 
     @staticmethod
-    def generate(activity, activity_sax_repr):
+    def generate(activity, activity_sax_repr, classNo):
         count = 1
-        for i in range(0, len(activity_sax_repr)//32, 1):
-            image_source = activity_sax_repr[32*i:32*(i+1)]
-            print("Image Letter Scheme #" + str(count) + ": " + image_source)
-            text_to_image.encode(image_source, "./pixel_bitmaps/train/" + activity + "/"
-                                 + activity + "0" + str(count) + ".png")
+        for i in range(0, len(activity_sax_repr)//64, 1):
+            image_source = activity_sax_repr[64*i:64*(i+1)]
+            print("image class: (" + str(classNo) + ") Activity: (" + activity + ") Representation" + ": " + image_source)
+            if i % 5 == 0:
+                text_to_image.encode(image_source, "./pixel_bitmaps/test/" + activity + "/"
+                                    + activity + "0" + str(classNo) + "-" + str(count) + ".png")
+            else:
+                text_to_image.encode(image_source, "./pixel_bitmaps/train/" + activity + "/"
+                                    + activity + "0" + str(classNo) + "-" + str(count) + ".png")
             count += 1
 
 def main():
