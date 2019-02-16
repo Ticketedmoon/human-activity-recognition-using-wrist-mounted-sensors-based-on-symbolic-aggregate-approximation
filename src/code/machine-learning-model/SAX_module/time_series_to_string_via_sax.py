1b5c25a00d1cd77a00d1b47ab7cc73e668c19c53
Shane Creedon <Shane.creedon3@mail.dcu.ie>
2019-02-16 19:08:44 +0000

    Wrote own SAX python module, no longer library incorporated

---
+++ src/code/machine-learning-model/SAX_module/time_series_to_string_via_sax.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+from SAX_module.sax_letter_conversion import letter_conversion
+
+class Time_series_to_string_via_sax:
+
+    def generate(self, file_path):
+        walk_data = pd.read_csv(file_path, index_col=0, parse_dates=True, skiprows=[0])
+        walk_stats = pd.Series(walk_data.values.squeeze())
+        numeric_data = pd.to_numeric(walk_stats, errors='coerce')
+        return numeric_data
+
+    def apply_letter_window(self, ts_data, window_size):
+        average_window_values = []
+        for i in range(0, len(ts_data)-window_size, window_size):
+            windowed_data = ts_data[i:i+window_size]
+            avg_win_val = sum(windowed_data.values) // window_size
+            average_window_values.append(avg_win_val)
+        return pd.Series(average_window_values)
+
+    def normalise_data(self, data, threshold=0.01):
+        sd = np.std(data)
+        if (sd < threshold):
+            return data
+        mean = np.mean(data)
+        return (data - mean) / sd
+
+    def ts_to_string(self, series, cuts):
+        a_size = len(cuts)
+        sax = list()
+        for i in range(0, len(series)):
+            num = series[i]
+            if(num >= 0):
+                j = a_size - 1
+                while ((j > 0) and (cuts[j] >= num)):
+                    j = j - 1
+                sax.append(self.idx2letter(j))
+            else:
+                j = 1
+                while (j < a_size and cuts[j] <= num):
+                    j = j + 1
+                sax.append(self.idx2letter(j-1))
+        return ''.join(sax)
+    
+    def idx2letter(self, idx):
+        if 0 <= idx < 20:
+            return chr(97 + idx)
+        else:
+            raise ValueError('A wrong idx value supplied.')
+
+    def generate_string_from_time_series(self, file_path_to_ts, letter_boundary_size, horizontal_window_size):
+        data = self.generate(file_path_to_ts)
+        # windowed_data = self.apply_letter_window(data, horizontal_window_size)
+        norm_data = self.normalise_data(data)
+        options = letter_conversion(letter_boundary_size)
+        sax_string = self.ts_to_string(norm_data, options)
+        return sax_string
--- src/code/machine-learning-model/symbolic_aggregate_approximation.py
+++ src/code/machine-learning-model/symbolic_aggregate_approximation.py
@@ -1,45 +1,31 @@
 import pandas as pd
-
-from saxpy.znorm import znorm
-from saxpy.sax import ts_to_string
-from saxpy.alphabet import cuts_for_asize
+from SAX_module.time_series_to_string_via_sax import Time_series_to_string_via_sax
 
 class SymbolicAggregateApproximation:
 
     exercise_path = "../../resources/exercise-datasets/"
 
+    def __init__(self):
+        self.sax_obj = Time_series_to_string_via_sax()
+
     def generate_walk(self, file_no):
-        walk_data = pd.read_csv(self.exercise_path + "Walk0" + str(file_no) + ".csv",
-                                index_col=0, parse_dates=True, skiprows=[0, 1])
-        walk_stats = pd.Series(walk_data.values.squeeze())
-        numeric_data = pd.to_numeric(walk_stats, errors='coerce')
-        return self.apply_sax_transformation(numeric_data)
+        walk_path = self.exercise_path + "Walk0" + str(file_no) + ".csv"
+        exercise_results = self.sax_obj.generate_string_from_time_series(walk_path, 20, 1)
+        return exercise_results
 
     def generate_run(self, file_no):
-        run_data = pd.read_csv(self.exercise_path + "Run0" + str(file_no) + ".csv",
-                               index_col=0, parse_dates=True, skiprows=[0, 1])
-        run_stats = pd.Series(run_data.values.squeeze())
-        numeric_data = pd.to_numeric(run_stats, errors='coerce')
-        return self.apply_sax_transformation(numeric_data)
+        run_path = self.exercise_path + "Run0" + str(file_no) + ".csv"
+        exercise_results = self.sax_obj.generate_string_from_time_series(run_path, 20, 1)
+        return exercise_results
 
     def generate_low_bike(self, file_no):
-        low_bike_data = pd.read_csv(self.exercise_path + "LowResistanceBike0" + str(file_no) + ".csv",
-                                    index_col=0, parse_dates=True, skiprows=[0, 1])
-        low_bike_stats = pd.Series(low_bike_data.values.squeeze())
-        numeric_data = pd.to_numeric(low_bike_stats, errors='coerce')
-        return self.apply_sax_transformation(numeric_data)
+        low_bike_path = self.exercise_path + "LowResistanceBike0" + str(file_no) + ".csv"
+        exercise_results = self.sax_obj.generate_string_from_time_series(low_bike_path, 20, 1)
+        return exercise_results
 
     def generate_high_bike(self, file_no):
-        high_bike_data = pd.read_csv(self.exercise_path + "HighResistanceBike0" + str(file_no) + ".csv",
-                                     index_col=0, parse_dates=True, skiprows=[0, 1])
-        high_bike_stats = pd.Series(high_bike_data.values.squeeze())
-        numeric_data = pd.to_numeric(high_bike_stats, errors='coerce')
-        return self.apply_sax_transformation(numeric_data)
-
-    @staticmethod
-    def apply_sax_transformation(data):
-        cut_size = cuts_for_asize(10)
-        exercise_results = ts_to_string(znorm(data), cut_size)
+        high_bike_path = self.exercise_path + "HighResistanceBike0" + str(file_no) + ".csv"
+        exercise_results = self.sax_obj.generate_string_from_time_series(high_bike_path, 20, 1)
         return exercise_results
 
 def main():
@@ -47,6 +33,7 @@ def main():
     print_demo(sax_obj, 1)
 
 def print_demo(sax_obj, file_no):
+
     try:
         print(" -- Walk Results --")
         print(sax_obj.generate_walk(file_no))
