3a363b0b4a62afbb8b19dd586c1b8c818e0f7819
Shane Creedon <Shane.creedon3@mail.dcu.ie>
2019-02-16 21:50:03 +0000

    minor change in python file sax

--- src/code/machine-learning-model/symbolic_aggregate_approximation.py
+++ src/code/machine-learning-model/symbolic_aggregate_approximation.py
@@ -10,22 +10,22 @@ class SymbolicAggregateApproximation:
 
     def generate_walk(self, file_no):
         walk_path = self.exercise_path + "Walk0" + str(file_no) + ".csv"
-        exercise_results = self.sax_obj.generate_string_from_time_series(walk_path, 20, 16)
+        exercise_results = self.sax_obj.generate_string_from_time_series(walk_path, 20, 1)
         return exercise_results
 
     def generate_run(self, file_no):
         run_path = self.exercise_path + "Run0" + str(file_no) + ".csv"
-        exercise_results = self.sax_obj.generate_string_from_time_series(run_path, 20, 16)
+        exercise_results = self.sax_obj.generate_string_from_time_series(run_path, 20, 1)
         return exercise_results
 
     def generate_low_bike(self, file_no):
-        low_bike_path = self.exercise_path + "LowResistanceBike0" + str(file_no) + ".csv"
-        exercise_results = self.sax_obj.generate_string_from_time_series(low_bike_path, 20, 16)
+        low_bike_path = self.exercise_path + "LowResistanceBike0" + str(file_no) + ".csv"#
+        exercise_results = self.sax_obj.generate_string_from_time_series(low_bike_path, 20, 1)
         return exercise_results
 
     def generate_high_bike(self, file_no):
         high_bike_path = self.exercise_path + "HighResistanceBike0" + str(file_no) + ".csv"
-        exercise_results = self.sax_obj.generate_string_from_time_series(high_bike_path, 20, 16)
+        exercise_results = self.sax_obj.generate_string_from_time_series(high_bike_path, 20, 1)
         return exercise_results
 
 def main():
