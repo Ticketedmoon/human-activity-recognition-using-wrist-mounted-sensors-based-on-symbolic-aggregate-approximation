0f5c1d7d4169cf60a667fe4ee01e769bdd135918
Shane Creedon <Shane.creedon3@mail.dcu.ie>
2019-04-17 22:27:48 +0100

    Fix arduino Graphing in real-time with python

--- src/code/arduino_module/initialize_arduino.py
+++ src/code/arduino_module/initialize_arduino.py
@@ -1,63 +1,49 @@
-from datetime import datetime
-from drawnow import *
-
-import serial
-import time
-import numpy as np              #import numpy
+import serial # import Serial Library
+import numpy  # Import numpy
 import matplotlib.pyplot as plt #import matplotlib library
+from drawnow import *
 
 class Arduino:
 
-    samples = []
-    microvolts = []
+    plt.ion() #Tell matplotlib you want interactive mode to plot live data
 
-    plt.ion()  # Tell matplotlib you want interactive mode to plot live data
-    axes = plt.gca()
+    def __init__(self):
+        self.fig, self.ax = plt.subplots()
+        self.samples, self.microvolts = [], []
+        self.ln = self.ax.plot(self.samples, self.microvolts, 'ro')
+        plt.show()
 
     def read_from_ppg(self):
         with serial.Serial('COM3', 19200, bytesize=serial.EIGHTBITS, timeout=0) as ser, open("voltages.csv", 'w') as text_file:
-            data_row_sample = 0
             text_file.write("{}, {}\n".format("Samples", "Microvolts(mV)"))
+            data_row_sample = 0
             while True:
-                voltage_reading = ser.readline().decode().strip("\n").strip("\r\n")
-                if (voltage_reading != ""):
-                    print(str(data_row_sample), voltage_reading) 
+                voltage_reading = str(ser.readline().decode(encoding='utf-8', errors='strict')).strip("\n").strip("\r\n")
+                if (voltage_reading != "" and voltage_reading.isdigit() and float(voltage_reading) > 100):
                     data = [str(data_row_sample), voltage_reading]
                     text_file.write("{}, {}\n".format(data_row_sample, voltage_reading))
                     text_file.flush()
-
-                    self.samples.append(data_row_sample)      #Build our temperatureF array by appending temp readings
-                    self.microvolts.append(voltage_reading)
-                    self.sample = voltage_reading
-
-                    drawnow(self.makeFig)                       #Call drawnow to update our live graph
-                    #plt.pause(.000000001)                     # Pause Briefly. Important to keep drawnow from crashing
-
-                    if(data_row_sample >= 50):                            #If you have 50 or more points, delete the first one from the array
-                        self.microvolts = []
-                        self.samples = []
-                        data_row_sample = 0
-
+                    self.samples.append(float(data_row_sample))
+                    self.microvolts.append(float(voltage_reading))
+                    drawnow(self.makeFig)
+                    #plt.pause(.000001)                     #Pause Briefly. Important to keep drawnow from crashing
                     data_row_sample += 1
+
+                    if(data_row_sample > 25):                            #If you have 50 or more points, delete the first one from the array
+                        self.samples.pop(0)                       #This allows us to just see the last 50 data points
+                        self.microvolts.pop(0)
+
                     ser.flushInput()
                     ser.flushOutput()
-                    #time.sleep(0.000001)
     
     def makeFig(self): # Create a function that makes our desired plot
-        plt.title('My Live Streaming Sensor Data')             # Plot the title
-        #plt.plot(np.random.randn(100), np.random.randn(100))
-        # plt.plot(self.samples, self.microvolts, '-')       # plot the temperature
-        # plt.axes([0, 50, 0, 2500])
-        # plt.legend(loc='upper left')                           # plot the legend
-        # plt.show()
-        plt.mpl_connect('close_event', handle_close)
-        plt.plot(self.samples, self.microvolts, '-')
-        plt.axis([0, 50, 0, 100])
-        plt.show()
-
-    def handle_close(self, evt):
-        evt.canvas.figure.axes[0].has_been_closed = True
-        print ('Closed Figure')
+        plt.ylim(0, 2500)                           # Set limits of second y axis- adjust to readings you are getting
+        plt.title('My Live Streaming Sensor Data')      # Plot the title
+        plt.grid(True)                                  # Turn the grid on
+        plt.ylabel('Microvolts(mV)')                    # Set ylabels
+        plt.xlabel('Samples')                           # Set ylabels
+        plt.plot(self.samples, self.microvolts, 'g-', label='Pulse Rate Microvolts(mV)')       #plot the temperature
+        plt.legend(loc='upper left')                    
 
 def main():
     ppg = Arduino()
